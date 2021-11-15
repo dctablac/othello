1dd1936e78d5dce4a41aae30b1ba3c114ac3a74f
Dan Tablac <dctablac@gmail.com>
2021-11-14 16:09:36 -0800

    Validation checks on user input. Pieces can be placed

--- othello_interface.py
+++ othello_interface.py
@@ -2,6 +2,21 @@
 import othello_logic
 
 
+def validate_board_size() -> int:
+    """ Validates the board size given or quits the game """
+    while True:
+        try:
+            board_size = input('Board size [4, 6, or 8] (or "quit" to quit): ')
+            if board_size == 'quit':
+                return -1
+            board_size = int(board_size)
+            if board_size < 4 or board_size > 8 or (board_size % 2 != 0):
+                raise ValueError
+            return board_size
+        except ValueError:
+            print('Please enter a board size of 4, 6, or 8')
+
+
 def print_board(board: [[str]]) -> None:
     """ Prints the game board to the console """
     # Column indices
@@ -21,7 +36,9 @@ def print_board(board: [[str]]) -> None:
 def game_loop():
     """ Runs the game loop """
     print('#----------OTHELLO----------#')
-    board_size = int(input('Board size: '))
+    board_size = validate_board_size()
+    if board_size == -1:
+        return
     game_board = othello_logic.set_game_board(board_size)
     game_info = othello_logic.default_game_info()
     error_msg = None
@@ -35,19 +52,22 @@ def game_loop():
             print(error_msg)
         if game_info.prev_turn is not None and error_msg is None:
             print(f'Placed {game_info.prev_turn} at ({game_info.prev_move.row},{game_info.prev_move.column})')
+        print(f'Turn: {game_info.curr_turn}')
         move = input('Please enter a move: [row] [col] (or "quit" to quit)\n')
         if move == 'quit':
-            print('Game quit. Game over!')
-            return
+            return print('Game quit. Game over!')
         try:
             move = move.split(' ')
             move_row, move_column = int(move[0]), int(move[1])
             if othello_logic.is_valid_move(game_board, move_row, move_column):
                 game_board = othello_logic.place_piece_on_board(game_board, move_row, move_column, game_info.curr_turn)
-                game_info = othello_logic.update_game_info(game_info, move_row, move_column, game_info.curr_turn)
-            error_msg = None
-        except:
+                black_count = othello_logic.count_player_pieces(game_board, othello_logic.P_BLACK)
+                white_count = othello_logic.count_player_pieces(game_board, othello_logic.P_WHITE)
+                game_info = othello_logic.update_game_info(game_info, move_row, move_column, black_count, white_count)
+                error_msg = None
+            else:
+                raise othello_logic.InvalidMoveError
+        except othello_logic.InvalidMoveError:
             error_msg = 'Invalid move.'
-        # if othello_logic.is_valid_move(move):
-        #     game_board = othello_logic.place_piece_on_board(game_board, move, game_info.turn)
-        #     # game_info = othello_logic.update_game_info()
+        except ValueError:
+            error_msg = "Invalid move. Please enter numbers in range 0-9"
--- othello_logic.py
+++ othello_logic.py
@@ -1,13 +1,16 @@
 # othello_logic.py
 from collections import namedtuple
 
-GameInfo = namedtuple('GameInfo', 'black_count white_count curr_turn winner is_game_over prev_turn prev_move')
+GameInfo = namedtuple('GameInfo', 'black_count white_count curr_turn winner is_game_over prev_move prev_turn')
 Move = namedtuple('Move', 'row column')
+
+P_BLACK = 'B'
+P_WHITE = 'W'
 
 
 def default_game_info() -> namedtuple:
     """ Returns namedtuple of game info
-        (black_count, white_count, turn, winner, is_game_over, prev_turn, prev_move) """
+        (black_count, white_count, curr_turn, winner, is_game_over, prev_move, prev_turn) """
     return GameInfo(2, 2, 'B', None, False, None, None)
 
 
@@ -21,16 +24,6 @@ def set_game_board(n: int) -> [[str]]:
     return board
 
 
-def _set_first_pieces(board: [[str]]) -> [[str]]:
-    """ Sets the initial pieces on the board """
-    half_board_length = len(board) // 2
-    board[half_board_length - 1][half_board_length - 1] = 'W'
-    board[half_board_length][half_board_length] = 'W'
-    board[half_board_length - 1][half_board_length] = 'B'
-    board[half_board_length][half_board_length - 1] = 'B'
-    return
-
-
 def place_piece_on_board(board: [[str]], row: int, column: int, turn: str) -> [[str]]:
     """ Places a piece on the game board """
     board[row][column] = turn
@@ -42,11 +35,71 @@ def flip_valid_pieces(board: [[str]], recent_move: str) -> [[str]]:
     pass
 
 
+def update_game_info(game_info: GameInfo, curr_move_row: int, curr_move_column: int,
+                     new_black_count: int, new_white_count: int, winner=None, is_game_over=False) -> GameInfo:
+    """ Returns update game info """
+    prev_move = Move(curr_move_row, curr_move_column);
+    return game_info._replace(
+        black_count=new_black_count,
+        white_count=new_white_count,
+        curr_turn=_flip_turn(game_info.curr_turn),
+        winner=winner,
+        is_game_over=is_game_over,
+        prev_move=prev_move,
+        prev_turn=game_info.curr_turn
+    )
+
+
+def count_player_pieces(board: [[str]], player: str) -> int:
+    """ Counts the pieces of [player] on the board"""
+    if player != P_WHITE and player != P_BLACK:
+        raise InvalidPlayerError('Player must be either B or W')
+    count = 0
+    for row in board:
+        for col in row:
+            if col == player:
+                count += 1
+    return count
+
+
+# Helper functions
+
+
+def _set_first_pieces(board: [[str]]) -> [[str]]:
+    """ Sets the initial pieces on the board """
+    half_board_length = len(board) // 2
+    board[half_board_length - 1][half_board_length - 1] = P_WHITE
+    board[half_board_length][half_board_length] = P_WHITE
+    board[half_board_length - 1][half_board_length] = P_BLACK
+    board[half_board_length][half_board_length - 1] = P_BLACK
+    return
+
+
+def _flip_turn(turn: str) -> str:
+    """ Flips the game turn """
+    return P_WHITE if turn == P_BLACK else P_BLACK
+
+
 # Validation functions
+
 
 def is_valid_move(board: [[str]], row: int, column: int) -> bool:
     """ Validates a move """
-    # Accept moves as row, col or (row, col)
+    board_len = len(board)
+    if (row < 0 or row > board_len - 1) or (column < 0 or column > board_len - 1):
+        raise InvalidMoveError
     if board[row][column] != '-':
-        return False
+        raise InvalidMoveError
+    # TODO: Extra checks
     return True
+
+
+# Exception classes
+
+
+class InvalidPlayerError(Exception):
+    pass
+
+
+class InvalidMoveError(Exception):
+    pass
